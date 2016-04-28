a0a03b5af6fe7b87d510fa946068f9e0d1c0225e
miixa <mixa@tdib.ru>
2016-04-28 19:42:59 +0300

    6

--- apptestov/urls.py
+++ apptestov/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^subjects/$', views.SubjectsView),
     url(r'^themes/get/(?P<url_subject_id>\d+)/$', views.ThemesView),
     url(r'^lessons/get/(?P<url_theme_id>\d+)/$', views.LessonsView),
+    url(r'^lesson/get/(?P<url_lesson_id>\d+)/$', views.LessonView),
 ]
--- apptestov/views.py
+++ apptestov/views.py
@@ -20,3 +20,13 @@ def LessonsView (request,url_theme_id):
     args['lessons'] = Lesson.objects.filter(theme_id=url_theme_id)
     return render_to_response('lessons.html',args)
 
+def LessonView (request,url_lesson_id):
+    subject = Subject.objects.filter(id=url_lesson_id)
+    theme = Theme.objects.filter(id=url_lesson_id)
+    lesson = Lesson.objects.filter(id=url_lesson_id)
+    args = {}
+    args['subject'] = subject[0]
+    args['theme'] = theme[0]
+    args['lesson'] = lesson[0]
+    return render_to_response('lesson.html',args)
+
